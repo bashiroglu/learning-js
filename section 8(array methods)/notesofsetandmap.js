8e026cecdf2fb4486e53012df2fb1ba2b47fab45
unknown <abdullabashir32@gmail.com>
2019-12-01 14:02:45 +0100

    practiced set and map, special version of set

---
+++ section 8(array methods)/notesofsetandmap.js
@@ -0,0 +1,48 @@
+// Sets are like array but unique values and with some additional
+// methods, also some array methods don't work here
+// const ids = new Set([1, 2, 4]); //get array
+// console.log(ids);
+// ids.add(5);
+
+// ids.delete(5);
+// if (ids.has(5)) {
+//   ids.delete(5);
+//   console.log('5 is here');
+// }
+
+// for (const entry of ids.entries()) {
+//   console.log(entry);
+// }
+
+// shift alt a -comment many lines, I changed it shift+ctrl+/
+const object1 = { name: 'Abdulla' };
+const object2 = { name: 'Orxan' };
+
+// const personData = new Map([
+//   [object1, [{ date: '10-09-2019', name: 't-shirt' }]]
+// ]);
+// personData.set(object2, [{ date: '10-09-2018', name: 'shirt' }]);
+// // console.log(personData.get(object1)[0].date);
+// console.log(personData);
+
+// for(const entry of personData.entries()){
+//     console.log(entry);
+// }
+// for (const [key, value] of personData.entries()) {
+//   console.log(key, value);
+// }
+
+// for (const key of personData.keys()) {
+//   console.log(key);
+// }
+// for (const value of personData.values()) {
+//   console.log(value);
+// }
+// personData.size is property show size) blue cube means property
+
+/* let person = { name: 'Abdulla' };
+const persons = new WeakSet();
+persons.add(person);
+
+person = null; */
+// weekset is mainly for garbage collector and advanced topic
