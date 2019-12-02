21bff01061b801e1d718aaf281efafbd049b9595
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-02 13:40:44 +0100

    added filter functionality and other things related to that

--- section 9/objects-01-starting-project/assets/scripts/objects.js
+++ section 9/objects-01-starting-project/assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
   const movieList = document.getElementById('movie-list');
 
   if (movies.length === 0) {
@@ -12,11 +12,21 @@ const renderMovies = () => {
   } else {
     movieList.classList.add('visible');
   }
-  movieList.innerHTML = '';
 
-  movies.forEach((movie) => {
+  movieList.innerHTML = '';
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter(movie => movie.info.title.includes(filter));
+
+    filteredMovies.forEach(movie => {
     const movieEl = document.createElement('li');
-    movieEl.textContent = movie.info.title;
+    let text = movie.info.title;
+    for (const key in movie.info) {
+      if (key !== 'title') {
+        text = text + `  ${key}:${movie.info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
     movieList.append(movieEl);
   });
 };
@@ -46,4 +56,12 @@ const addMovieHandler = () => {
   renderMovies();
 };
 
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
+  console.log(filterTerm);
+  
+};
+
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
