f60862b267c3c82a32dc2abbdea3669d9d1b002c
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-05 02:24:50 +0100

    started section 10 objects, practiced with classes, methods, fields, added constructor method, creeting object using class and giving arguments by using its constructur function arguments

---
+++ section 10/assets/scripts/app.js
@@ -0,0 +1,66 @@
+class Product {
+  //   title = 'Default';
+  //   imageUrl;
+  //   description;
+  //   price;
+
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+}
+// console.log(new Product());
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+    return prodEl;
+  }
+}
+class ProductList {
+  products = [
+    new Product(
+      'A Pillow',
+      'https://app.skufetch.com/images.tmp/TrafficMASTER_Assort_2422_0_res.jpg',
+      'A soft pillow!',
+      19.99
+    ),
+    new Product(
+      'A Carpet',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
+      'A carpet which you might like - or not.',
+      89.99
+    )
+  ];
+  render() {
+    const renderHook = document.getElementById('app');
+    const prodList = document.createElement('ul');
+    prodList.className = 'product-list';
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  }
+}
+
+const productList = new ProductList();
+productList.render();
